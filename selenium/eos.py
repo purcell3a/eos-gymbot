aa95ee6f5965483616e9828b2c6f3ec20e5a1392
Angela Purcell <kalmpurcell@gmail.com>
2021-03-26 16:29:57 -0700

    initial commit

---
+++ selenium/eos.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import csv
+import time
+
+DRIVER_PATH = '/Users/mouse/src/chromedriver'
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver.get('https://mico.myiclubonline.com/iclub/members/signin?clubNumber=8667')
+
+time.sleep(3)
+
+
+# getting the button by class name
+# button = driver.find_element_by_class_name("slide-out-btn")
+# loginButtonID = 'cta_button_4595896_6b7c46e1-50d8-4c3a-a056-3100a5436ba3'
+# button = driver.find_element_by_id(loginButtonID)
+ 
+# clicking on the button
+# button.click()
+# print ("###########################  Page URL ########################### ")
+# # print(driver.current_url) #to get the current url (can be useful when there are redirections on the website and that you need the final URL)
+# print ("###########################  Page Title ########################### ")
+# # print(driver.title) #to get the page's title
+# print ("###########################  Page Source ########################### ")
+# # print(driver.page_source)   #will return and print the full page HTML code in terminal
+# print ("###########################  END Scraping ########################### ")
+# # driver.quit()
