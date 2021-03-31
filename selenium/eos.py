6c59390e6991da457e996c1bf53a80b8918a86fd
Angela Purcell <kalmpurcell@gmail.com>
2021-03-31 11:57:53 -0700

    form element is hidden

--- selenium/eos.py
+++ selenium/eos.py
@@ -1,25 +1,63 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
 import csv
 import time
 import secrets
 import requests
-# DRIVER_PATH = '/Users/mouse/src/chromedriver'
-# driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-# driver.get('https://mico.myiclubonline.com/iclub/members/signin?clubNumber=8667')
+
+DRIVER_PATH = '/Users/mouse/src/chromedriver'
+
+
+options = Options()
+options.headless = True
+options.add_argument("--window-size=1920,1200")
+
+
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver.get('https://eosfitness.com/schedule-workout-select-club-ca/')
+
+time.sleep(3)
+WebDriverWait(driver, 20)
+
+#* SELECT INPUT BOXED BY ID:
+select = Select(driver.find_element_by_id('input_74_1'))
+WebDriverWait(driver, 20)
+# select by visible text
+select.select_by_visible_text('Los Angeles- Downtown LA/Cesar Chavez')
+time.sleep(3)
+form_element = driver.find_element_by_xpath("html/body/div//form/div[2]")
+l = form_element.get_attribute('innerHTML')
+
+# button = driver.find_element_by_class_name('wpb_wrapper')
+# oteh = driver.find_element_by_id("step-pick-appointment")
+print(l)
+
+# select by value 
+# select.select_by_value('1')
+# inputElement = driver.find_element_by_id("username")
+# inputElement.send_keys('purcell3a')
 
 # time.sleep(3)
-# repo = 'eos-gymbot'
-# owner = 'purcell3a'
-# r = requests.get('https://api.github.com/repos/purcell3a/eos-gymbot/actions/secrets')
-# f = r.json()
 
+# inputElement = driver.find_element_by_id("password")
+# inputElement.send_keys('L!0j26f13')
+#* Now you can simulate hitting ENTER:
+# time.sleep(3)
 
+#* or if it is a form you can submit:
+
+# inputElement.submit() 
 # getting the button by class name
-# button = driver.find_element_by_class_name("slide-out-btn")
+# button = driver.find_element_by_class_name("select-item-box select-item")
 # loginButtonID = 'cta_button_4595896_6b7c46e1-50d8-4c3a-a056-3100a5436ba3'
 # button = driver.find_element_by_id(loginButtonID)
- 
+# button = driver.find_element_by_id("step-pick-appointment")
+
 # clicking on the button
 # button.click()
 # print ("###########################  Page URL ########################### ")
